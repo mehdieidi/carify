c0096c1fb1e00afeb2fb3bf0e28d8c04a1cf7562
Mehdi Eidi <mehdiadq@gmail.com>
2023-01-04 10:24:22 +0330

    refactor: refactore plot code

--- plot/linear_regression.py
+++ plot/linear_regression.py
@@ -1,49 +1,61 @@
 import pandas as pd
 import matplotlib.pyplot as plt  
 import seaborn as seabornInstance 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from joblib import dump
+import numpy as np
+from bokeh.plotting import figure, show, output_file
 
+
+## Linear Regression Training ##
 df = pd.read_csv("../pycsv/pcars_data.csv")
-
-plt.figure(figsize=(15,10))
-plt.tight_layout()
-seabornInstance.distplot(df["cash_cost"])
-
-# Remove the "cost" column because it is not a feature but a label.
-# Also remove the "id" column and "token" column that are useless in model training.
 x = df.drop(labels="cash_cost", axis=1).iloc[:,2:-1]
 y = df['cash_cost']
-
-from sklearn.model_selection import train_test_split
-
-# random_state as the name suggests, is used for initializing the internal random number generator,
-# which will decide the splitting of data into train and test indices in your case
-X_train, X_test, y_train, y_test = train_test_split(x,y , test_size=0.2, shuffle=True)
-
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
-
+X_train, x_test, y_train, y_test = train_test_split(x,y , test_size=0.2, shuffle=True)
 regressor = LinearRegression() 
 regressor.fit(X_train.values,y_train)
+y_pred = regressor.predict(x_test)
+mae1 = mean_absolute_error(y_test, y_pred)
+print(mae1)
+#################################
 
-y_pred = regressor.predict(X_test)
-print(mean_absolute_error(y_test, y_pred))
+## PLOT  2 ##
+plt.figure(figsize=(10,10))
+plt.scatter(y_test, y_pred, c='crimson')
+plt.yscale('log')
+plt.xscale('log')
 
-from joblib import dump
+p1 = max(max(y_pred), max(y_test))
+p2 = min(min(y_pred), min(y_test))
+plt.plot([p1, p2], [p1, p2], 'b-')
+plt.xlabel('True Values', fontsize=15)
+plt.ylabel('Predictions', fontsize=15)
+plt.axis('equal')
+plt.show()
+##################################
 
-dump(regressor, '../model/model.joblib') 
 
-import numpy as np
+## Decision Tree Training ##
+from sklearn.tree import DecisionTreeRegressor 
+regressor = DecisionTreeRegressor(criterion='absolute_error')
+regressor.fit(X_train.values,y_train)
+y_pred2 = regressor.predict(x_test)
+mae2 = mean_absolute_error(y_test, y_pred2)
+print(mae2)
+###################################
 
+## PLOT 1 ##
 y_test = np.array(list(y_test))
 y_pred = np.array(y_pred)
+y_pred2 = np.array(y_pred2)
 
-df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+df = pd.DataFrame({'Actual': y_test.flatten(), 'Linear Regression Predicted': y_pred.flatten(),  'Decision Tree Predicted': y_pred2.flatten()})
 df1 = df.head(25)
 df1.plot(kind='bar',figsize=(16,10))
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
+##################################
 
-plt.scatter(X_test, y_test,  color='gray')
-plt.plot(X_test, y_pred, color='red', linewidth=2)
-plt.show()
